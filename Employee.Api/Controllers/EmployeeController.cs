f2dea6ff92c32bf3013fb28197481c207041d23d
Beste Karagöz <30674420+bestekrgz@users.noreply.github.com>
2022-11-19 16:36:25 +0300

    Add files via upload

---
+++ Employee.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using Employee.Service.Abstracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Employee.Domain.Entities;
+using System.Threading.Tasks;
+using Employee.Service.Dtos;
+
+namespace Employee.Api.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeDto employeedto)
+        {
+            await employeeService.Add(employeedto);
+            return Ok();
+        }
+        [HttpGet("List")] //TODO
+
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var result = await employeeService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public  async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var result =await employeeService.GetById(id);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(EmployeeDto employeedto,int id)
+        {
+            await employeeService.Update(employeedto,id);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            await employeeService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Employee.Api/Middlewares/LogMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Product.API.Middlewares
+{
+    public class LogMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LogMiddleware> _logger;
+
+        public LogMiddleware(RequestDelegate next, ILogger<LogMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+   
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+            var watch = Stopwatch.StartNew();
+
+            await _next.Invoke(httpContext);
+            watch.Stop();
+            if (watch.ElapsedMilliseconds > 500)
+            {
+                _logger.LogTrace("Duration:{duration}ms, Request path:{path},Request Method:{method}",
+                    watch.ElapsedMilliseconds, httpContext.Request.Path, httpContext.Request.Method);
+            }
+        }
+    }
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLogMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LogMiddleware>();
+        }
+    }
+}
